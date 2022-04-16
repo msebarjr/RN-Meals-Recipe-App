20bef8a68b8d78f16a325ac313f18edc0e6d5ded
Michael Sebar Jr <msebarjr@gmail.com>
2022-04-15 23:05:23 -0400

    Setup environment

---
+++ screens/MealCategories.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MealCategories() {
+    return (
+        <View>
+            <Text>MealCategory</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
